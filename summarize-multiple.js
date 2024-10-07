7ddfbf8bc30f97740b439ff711ba4270ca973ff1
Nic Jansma <nic@nicj.net>
2024-10-07 18:51:48 -0400

    Added summarize.js and summarize-multiple.js

---
+++ summarize-multiple.js
@@ -0,0 +1,115 @@
+//
+// Summarizes subdirectories' runs
+//
+// Run from the outputDir directory that contains multiple runs.
+//
+
+//
+// Imports
+//
+import fs from "fs";
+import path from "path";
+import { median, stdev } from "stats-lite";
+
+let dirs = fs.readdirSync(".");
+
+let audits = [];
+
+if (typeof process.argv[2] !== "undefined") {
+    // single audit
+    audits.push(process.argv[2]);
+} else {
+    // find which audits are available
+
+    // find a file with a list of audits
+    let files = fs.readdirSync(dirs[0]);
+
+    for (let file in files) {
+        if (files[file].indexOf(".jpg") !== -1 ||
+            files[file].indexOf("-trace.json") !== -1 ||
+            files[file].indexOf("-artifacts.json") !== -1) {
+            continue;
+        }
+
+        let json = fs.readFileSync(path.join(dirs[0], files[file]));
+        let obj = JSON.parse(JSON.parse(json));
+
+        // use all audits from this file
+        audits = Object.keys(obj.audits);
+
+        console.log(`Found audits: ${audits.join(",")}`);
+
+        // don't need to look any more
+        break;
+    }
+}
+
+let values = {};
+
+// find all directories
+dirs.forEach(function(dir) {
+    let files = fs.readdirSync(dir);
+
+    for (let file in files) {
+        if (files[file].indexOf(".jpg") !== -1 ||
+            files[file].indexOf("-trace.json") !== -1 ||
+            files[file].indexOf("-artifacts.json") !== -1) {
+            continue;
+        }
+
+        // read the JSON file and parse
+        let json = fs.readFileSync(path.join(dir, files[file]));
+        let obj = JSON.parse(JSON.parse(json));
+
+        // append any data we find
+        audits.forEach(function(auditName) {
+            values[auditName] = values[auditName] || {};
+            values[auditName][dir] = values[auditName][dir] || [];
+
+            if (!obj.audits[auditName].numericValue) {
+                // no value
+                return;
+            }
+
+            // push to this dir's results
+            values[auditName][dir].push(obj.audits[auditName].numericValue.toFixed(0));
+        });
+    }
+});
+
+// summarize all runs
+audits.forEach(function(auditName) {
+    let vals = values[auditName];
+
+    if (!vals[Object.keys(vals)[0]].length) {
+        return;
+    }
+
+    console.log();
+    console.log(auditName);
+
+    let maxLen = 0;
+    dirs.forEach(function(dir) {
+        maxLen = dir.length > maxLen ? dir.length : maxLen;
+    });
+
+    dirs.forEach(function(dir) {
+        console.log(`${dir.padStart(maxLen)}: ` +
+            `${median(vals[dir]).toFixed(0)} ` +
+            `+/- ${parseFloat(stdev(vals[dir])).toFixed(0)}`);
+    });
+
+    if (dirs.length === 2) {
+        let dir1 = Object.keys(vals)[0];
+        let dir2 = Object.keys(vals)[1];
+
+        let diffN = median(vals[dir1]) - median(vals[dir2]);
+        let diffPct = (diffN / median(vals[dir1])) * 100;
+
+        if (isNaN(diffPct)) {
+            diffPct = 0;
+        }
+
+        console.log(`${"diff".padStart(maxLen)}: ${(diffN).toFixed(0)} (${diffPct.toFixed(1)}%)`);
+    }
+});
---
+++ summarize.js
@@ -0,0 +1,85 @@
+//
+// Summarizes a Lighthouse runs' scores.
+//
+// Run from the final outputDir directory for a run.
+//
+
+//
+// Imports
+//
+import fs from "fs";
+import { median, stdev } from "stats-lite";
+
+let audits = [];
+
+if (typeof process.argv[2] !== "undefined") {
+    // single audit
+    audits.push(process.argv[2]);
+} else {
+    // find which audits are available
+
+    // find a file with a list of audits
+    let files = fs.readdirSync(".");
+
+    for (let file in files) {
+        if (files[file].indexOf(".jpg") !== -1 ||
+            files[file].indexOf("-trace.json") !== -1 ||
+            files[file].indexOf("-artifacts.json") !== -1) {
+            continue;
+        }
+
+        let json = fs.readFileSync(files[file]);
+        let obj = JSON.parse(JSON.parse(json));
+
+        // use all audits from this file
+        audits = Object.keys(obj.audits);
+
+        console.log(`Found audits: ${audits.join(",")}`);
+
+        // don't need to look any more
+        break;
+    }
+}
+
+// read all files in this directory
+let files = fs.readdirSync(".");
+let values = {};
+
+for (let file in files) {
+    if (files[file].indexOf(".jpg") !== -1 ||
+        files[file].indexOf("-trace.json") !== -1 ||
+        files[file].indexOf("-artifacts.json") !== -1) {
+        continue;
+    }
+
+    // read the JSON file and parse
+    let json = fs.readFileSync(files[file]);
+    let obj = JSON.parse(JSON.parse(json));
+
+    // append any data we find
+    audits.forEach(function(auditName) {
+        values[auditName] = values[auditName] || [];
+
+        if (!obj.audits[auditName].numericValue) {
+            // no value
+            return;
+        }
+
+        values[auditName].push(obj.audits[auditName].numericValue.toFixed(0));
+    });
+}
+
+// summarize all audits
+audits.forEach(function(auditName) {
+    let vals = values[auditName];
+
+    if (!vals.length) {
+        return;
+    }
+
+    console.log();
+    console.log(auditName);
+
+    console.log(`\tmedian:  ${median(vals).toFixed(0)} +/- ${parseFloat(stdev(vals)).toFixed(0)}`);
+    console.log(`\tresults: ${vals.join(", ")}`);
+});
